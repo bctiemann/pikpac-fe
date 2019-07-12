90fac2c2e5adb1a8c046989cb8503956fe312aff
Brian Tiemann <btman@mac.com>
2019-07-11 22:50:01 -0400

    Fill out box detail form page

--- pages/design/_product.vue
+++ pages/design/_product.vue
@@ -10,15 +10,108 @@
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mattis pharetra ultrices. Sed tempor pharetra eros, a lacinia sapien pellentesque.
     </p>
     <b-breadcrumb :items="productBreadcrumbs" />
-    <b-row v-if="selectedStyle">
-      <b-col sm="7">
-        <b-img :src="selectedStyle.image" fluid :alt="selectedStyle.verboseName" class="mb-5" />
-        <b-img :src="blisterTrayImageUrl" fluid :alt="selectedStyle.verboseName" />
-      </b-col>
-      <b-col>
-        blah details
-      </b-col>
-    </b-row>
+    <div v-if="selectedStyle">
+      <b-row>
+        <b-col sm="7">
+          <b-img :src="selectedStyle.image" fluid :alt="selectedStyle.verboseName" class="mb-5" />
+        </b-col>
+        <b-col>
+          blah details
+          <p class="page-blurb">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mattis pharetra ultrices. Sed tempor pharetra eros, a lacinia sapien pellentesque.
+          </p>
+          <b-form-select v-model="size" :options="sizeOptions" />
+          <b-form-select v-model="colors" :options="colorsOptions" />
+          <b-form-select v-model="finishing" :options="finishingOptions" />
+          <b-form-select v-model="boxQuantity" :options="boxQuantityOptions" />
+          <b-row>
+            <b-col>
+              Price
+            </b-col>
+            <b-col class="text-right">
+              {{ boxPrice || '' }}
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col sm="7">
+          <b-img :src="blisterTrayImageUrl" fluid :alt="selectedStyle.verboseName" class="mb-5" />
+        </b-col>
+        <b-col>
+          <h2>Blister Tray</h2>
+          <p class="page-blurb">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mattis pharetra ultrices. Sed tempor pharetra eros, a lacinia sapien pellentesque.
+          </p>
+          <b-form-select v-model="blisterTrayQuantity" :options="blisterTrayQuantityOptions" />
+          <b-row>
+            <b-col>
+              Price
+            </b-col>
+            <b-col class="text-right">
+              {{ blisterTrayPrice || '' }}
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+      <b-row class="">
+        <b-col>
+          <h2>Specifications</h2>
+          <b-row>
+            <div class="specification-header">
+              Material
+            </div>
+            <p>
+              {{ material }}
+            </p>
+          </b-row>
+          <b-row>
+            <div class="specification-header">
+              Material Color
+            </div>
+            <p>
+              {{ materialColor }}
+            </p>
+          </b-row>
+          <b-row>
+            <div class="specification-header">
+              Lamination
+            </div>
+            <p>
+              {{ lamination }}
+            </p>
+          </b-row>
+          <b-row>
+            <div class="specification-header">
+              Custom Printing
+            </div>
+            <p>
+              {{ customPrinting }}
+            </p>
+          </b-row>
+        </b-col>
+        <b-col sm="7">
+          <b-img :src="selectedStyle.image" fluid :alt="selectedStyle.verboseName" class="mb-5" />
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col />
+        <b-col>
+          <b-button>
+            Select from our preset designs
+          </b-button>
+        </b-col>
+        <b-col>
+          <b-button>
+            Upload your own design
+          </b-button>
+        </b-col>
+        <b-col />
+      </b-row>
+      <b-row>
+        <h2>Examples</h2>
+      </b-row>
+    </div>
     <b-row v-else-if="selectedProduct">
       <div v-for="style in selectedProduct.styles" :key="style.name" class="w-50 p-3" @click="selectStyle(style)">
         <b-img :src="style.image" fluid :alt="style.verboseName" />
@@ -47,7 +140,53 @@ export default {
       selectedProduct: null,
       selectedStyle: null,
       productBreadcrumbs: [ productBreadcrumbBase ],
-      blisterTrayImageUrl: blisterTrayImageUrl
+      blisterTrayImageUrl: blisterTrayImageUrl,
+      size: null,
+      sizeOptions: [
+        { value: null, text: 'Select size' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Selected Option' },
+        { value: { C: '3PO' }, text: 'This is an option with object value' },
+        { value: 'd', text: 'This one is disabled', disabled: true }
+      ],
+      colors: null,
+      colorsOptions: [
+        { value: null, text: 'Select # of colors' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Selected Option' },
+        { value: { C: '3PO' }, text: 'This is an option with object value' },
+        { value: 'd', text: 'This one is disabled', disabled: true }
+      ],
+      finishing: null,
+      finishingOptions: [
+        { value: null, text: 'Select finishing' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Selected Option' },
+        { value: { C: '3PO' }, text: 'This is an option with object value' },
+        { value: 'd', text: 'This one is disabled', disabled: true }
+      ],
+      boxQuantity: null,
+      boxQuantityOptions: [
+        { value: null, text: 'Select quantity' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Selected Option' },
+        { value: { C: '3PO' }, text: 'This is an option with object value' },
+        { value: 'd', text: 'This one is disabled', disabled: true }
+      ],
+      boxPrice: 3.5,
+      blisterTrayQuantity: null,
+      blisterTrayQuantityOptions: [
+        { value: null, text: 'Select quantity' },
+        { value: 'a', text: 'This is First option' },
+        { value: 'b', text: 'Selected Option' },
+        { value: { C: '3PO' }, text: 'This is an option with object value' },
+        { value: 'd', text: 'This one is disabled', disabled: true }
+      ],
+      blisterTrayPrice: 7.21,
+      material: '1.5mm Chipboard + 157 gsm Artboard',
+      materialColor: 'White',
+      lamination: 'Clear varnish / Exterior of the box',
+      customPrinting: 'CMYK 4 color process print'
     };
   },
 
