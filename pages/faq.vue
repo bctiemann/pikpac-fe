188fadfc144aabf1015f711319d02fec945cf7f4
Brian Tiemann <btman@mac.com>
2019-07-28 20:29:11 -0400

    FAQ and About pages

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,10 +1,37 @@
 <template>
   <div class="container">
-    FAQ
+    <b-row>
+      <b-col sm="4">
+        <ul>
+          <li v-for="category in categories" :key="category.id" @click="getHeadings({ category: category.id })">
+            {{ category.name }}
+          </li>
+          <li>
+            Contact us
+          </li>
+        </ul>
+      </b-col>
+      <b-col sm="8">
+        <h1>FAQ</h1>
+        <div v-for="heading in headings" :key="heading.id" class="heading">
+          {{ heading.title }}
+          <ul>
+            <li v-for="item in heading.items" :key="item.id">
+              {{ item.title }}
+              <div class="hidden">
+                {{ item.body }}
+              </div>
+            </li>
+          </ul>
+        </div>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+
 export default {
   components: {},
 
@@ -17,10 +44,25 @@ export default {
     };
   },
 
+  computed: {
+    ...mapState('faq', [
+      'categories',
+      'headings',
+      'items'
+    ])
+  },
+
+  async mounted () {
+    await this.getCategories();
+    await this.getHeadings({ category: 1 });
+  },
+
   methods: {
-    refreshPrice (item) {
-      console.log('foo');
-    }
+    ...mapActions('faq', [
+      'getCategories',
+      'getHeadings',
+      'getItems'
+    ])
   }
 
 };
---
+++ store/faq.js
@@ -0,0 +1,42 @@
+export const state = () => ({
+  categories: [],
+  headings: [],
+  items: [],
+  isLoading: false
+});
+
+export const mutations = {
+  setCategories (state, categories) {
+    state.categories = categories;
+  },
+  setHeadings (state, headings) {
+    state.headings = headings;
+  },
+  setItems (state, items) {
+    state.items = items;
+  },
+  setIsLoading (state, isLoading) {
+    state.isLoading = isLoading;
+  }
+};
+
+export const actions = {
+  async getCategories ({ commit }) {
+    commit('setIsLoading', true);
+    const { data } = await this.$axios.get(`/faq_categories/`);
+    commit('setCategories', data);
+    commit('setIsLoading', false);
+  },
+  async getHeadings ({ commit }, payload) {
+    commit('setIsLoading', true);
+    const { data } = await this.$axios.get(`/faq_headings/?category=${payload.category}`);
+    commit('setHeadings', data);
+    commit('setIsLoading', false);
+  },
+  async getItems ({ commit }, payload) {
+    commit('setIsLoading', true);
+    const { data } = await this.$axios.get(`/faq_items/?heading=${payload.heading}`);
+    commit('setItems', data);
+    commit('setIsLoading', false);
+  }
+};
