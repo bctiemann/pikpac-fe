47bd3db87dfa5b2c9f3908008a2d023fe917786f
Brian Tiemann <btman@mac.com>
2019-08-04 22:47:47 -0400

    Move auth navbar to component

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,38 +1,6 @@
 <template>
   <b-container>
-    <ul class="nav nav-pills nav-fill">
-      <nuxt-link class="logo" to="/">
-        pikpac
-      </nuxt-link>
-      <li class="nav-item">
-        <nuxt-link class="nav-link" to="/design" data-toggle="pill">
-          Design a box
-        </nuxt-link>
-      </li>
-      <li class="nav-item">
-        <nuxt-link class="nav-link" to="/how-it-works" data-toggle="pill">
-          How it works
-        </nuxt-link>
-      </li>
-      <li class="nav-item">
-        <nuxt-link class="nav-link" to="/faq" data-toggle="pill">
-          FAQ
-        </nuxt-link>
-      </li>
-      <li class="nav-item">
-        <nuxt-link class="nav-link" to="/account" data-toggle="pill">
-          My account
-        </nuxt-link>
-      </li>
-      <li v-if="user" class="nav-item">
-        <a class="nav-link" href="#"><font-awesome-icon icon="shopping-basket" /></a>
-      </li>
-      <li v-if="user" class="nav-item">
-        <nuxt-link class="nav-link" to="/auth/sign-out">
-          <font-awesome-icon icon="sign-out-alt" />
-        </nuxt-link>
-      </li>
-    </ul>
+    <Navbar />
     <b-row row align-center justify-center>
       <b-col />
       <b-col sm="6">
@@ -49,10 +17,12 @@
 
 <script>
 // import PrimarySnackbar from '~/components/PrimarySnackbar.vue';
+import Navbar from '~/components/Navbar.vue';
 
 export default {
   components: {
     // PrimarySnackbar
+    Navbar
   },
 
   computed: {
