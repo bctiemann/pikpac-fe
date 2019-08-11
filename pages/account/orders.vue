3345539c206b517020066f214999a0a51eae7c17
Brian Tiemann <btman@mac.com>
2019-08-11 00:35:02 -0400

    Add in-progress orders to cart from account/orders page

--- pages/account/orders.vue
+++ pages/account/orders.vue
@@ -3,21 +3,29 @@
     My Orders
     <ul>
       <li v-for="order in orders" :key="order.id">
+        <p>{{ order.project.title }}</p>
+        <b-btn @click="showModalAndAddToCart(order.project)">
+          Add to Cart
+        </b-btn>
         <p>{{ order.project.product.name }} {{ order.date_created }}</p>
         <b-img :src="order.project.product.picture" fluid :alt="order.project.product.name" />
       </li>
     </ul>
+    <AddedToCartModal />
   </div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
+import AddedToCartModal from '~/components/AddedToCartModal.vue';
 
 export default {
   layout: 'account',
   middleware: 'auth',
 
-  components: {},
+  components: {
+    AddedToCartModal
+  },
 
   data: () => {
     return {
@@ -43,6 +51,15 @@ export default {
     ...mapActions('orders', [
       'getOrders'
     ]),
+
+    ...mapActions('cart', [
+      'addToCart'
+    ]),
+
+    showModalAndAddToCart (project) {
+      this.$bvModal.show('added-to-cart');
+      this.addToCart(project);
+    },
 
     refreshPrice (item) {
       console.log('foo');
