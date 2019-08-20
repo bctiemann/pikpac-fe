54c5b93fa50e0ca24c0934ff122d6b744e30cfc2
Brian Tiemann <btman@mac.com>
2019-08-20 19:29:54 -0400

    Styling on orders page

--- pages/account/orders.vue
+++ pages/account/orders.vue
@@ -1,22 +1,53 @@
 <template>
   <div class="container">
     My Orders
-    <ul>
-      <li v-for="order in orders" :key="order.id">
-        <p @click="goToProject(order.project.id)">
-          {{ order.project.title }}
-        </p>
-        <b-btn @click="showModalAndAddToCart(order.project)">
-          Add to Cart
-        </b-btn>
-        <p>{{ calculatedPrice(order.project.unit_price) }} &times; {{ order.project.quantity }} = {{ calculatedPrice(order.project.unit_price * order.project.quantity) }}</p>
-        <p>{{ order.project.product.name }} {{ order.date_created }}</p>
-        <b-img :src="order.project.product.picture" fluid :alt="order.project.product.name" />
+    <ul class="orders">
+      <li v-for="order in orders" :key="order.id" @click="goToProject(order.project.id)">
+        <div class="container">
+          <b-row>
+            <b-col sm="3">
+              <b-img :src="order.project.product.picture" fluid :alt="order.project.product.name" />
+            </b-col>
+            <b-col>
+              <h4>{{ order.project.title }}</h4>
+              <p>{{ calculatedPrice(order.project.unit_price) }} &times; {{ order.project.quantity }} = {{ calculatedPrice(order.project.unit_price * order.project.quantity) }}</p>
+              <p>{{ order.project.product.name }} {{ order.date_created }}</p>
+            </b-col>
+            <b-col sm="2">
+              <b-btn @click.stop="showModalAndAddToCart(order.project)">
+                Add to Cart
+              </b-btn>
+            </b-col>
+          </b-row>
+        </div>
       </li>
     </ul>
     <AddedToCartModal />
   </div>
 </template>
+
+<style>
+.orders {
+  list-style-type: none;
+  padding: 0px;
+}
+.orders li {
+  border: 1px solid #000;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+/*
+.orders li img {
+  max-width: 120px;
+  float: left;
+}
+.order-item-info {
+  float: left;
+}
+*/
+</style>
 
 <script>
 import { mapState, mapActions } from 'vuex';
